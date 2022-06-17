b66f86e9c82dd893e6b6d53c1e13e8ac15dd8a6c
szyrelle <szyrellegalupo@gmail.com>
2022-06-17 22:16:30 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connection = require("./db");
+const path = require("path");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+
+// database connection
+connection();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, console.log(`Listening on port ${port}...`));
